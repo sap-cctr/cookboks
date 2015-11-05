8846751ba1c563b88a1cee2a797a673f97f781d3
sap-cctr <zdenko@freemail.hu>
2015-11-05 09:09:59 +0000

    Added 2 recipes

---
+++ test03/recipes/test01.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: test03
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+Chef::Log.info "chef::#{cookbook_name}::#{recipe_name}"
